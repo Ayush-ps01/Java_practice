953820f1e283ccca9f82c742217a02b3c8af9b54
Ayush-ps01 <ayush.singh_cs.aiml24@gla.ac.in>
2026-02-12 14:56:27 +0530

    Questing move zeros to end using two pointer

--- q10.java
+++ q10.java
@@ -1,8 +1,29 @@
-//move zeros to end of zeros
-
-
+//move zeros to end of array
+import java.util.*;
 public class q10 {
-    static void main() {
+    public static void MoveZeros(int[] arr){
+        int slow = 0;
+        for(int fast=0;fast<arr.length;fast++){
+            if(arr[fast]!=0){
+                int temp = arr[slow];
+                arr[slow]=arr[fast];
+                arr[fast]=temp;
+
+                slow++;
+            }
+        }
+    }
+    public static void main() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr= new int[n];
+        for(int i = 0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        MoveZeros(arr);
+        for(int num:arr){
+            System.out.println(num+" ");
+        }
 
     }
 }
